9c2714421314ebfd7b821970a01fee537171d3d0
snforge <60017569+snforge@users.noreply.github.com>
2020-04-13 12:18:15 -0700

    Make last bar fully visible on charts, closes #3

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-
 import backendAPI from '../components/backendapi';
 import Dygraphs from '../components/Dygraphs.vue';
 
@@ -47,27 +46,32 @@ export default {
     this.getData();
   },
   methods: {
-     async getData() {
+    async getData() {
       console.log('Got response!');
       let response = await backendAPI.getUSTrendData(this.filename);
-      if(response.success){
+      if (response.success) {
         this.localChartOptions.labels = response.data.labels;
         let chartData = [];
         // Convert X to Date
-        if(Array.isArray(response.data.data)){
-          chartData = response.data.data.map( x => [new Date(x[0]),x[1]]);
+        if (Array.isArray(response.data.data)) {
+          chartData = response.data.data.map(x => [new Date(x[0]), x[1]]);
+          // Push one more time step (day or month) with empty value into data, so last bar is fully visible
+          let prevTs = response.data.data[response.data.data.length - 2][0];
+          let lastTs = response.data.data[response.data.data.length - 1][0];
+          let timeStep = (lastTs-prevTs);
+          chartData.push([new Date(lastTs+timeStep), null]);
         }
         this.localChartData = chartData;
       }
       console.log('Got response!');
     }
   }
-}
+};
 </script>
 
 <style>
 .BarChart {
   padding: 20px;
-  background: #FFF;
+  background: #fff;
 }
 </style>
