c962bb6493dfc596e2309876faf73cb4150404ca
melmot55 <slava.zhakov@gmail.com>
2020-04-14 20:40:31 -0700

    moved teat

--- tools/dataprocessor.test.js
+++
@@ -1,9 +0,0 @@
-const dp = require('./dataprocessor');
-
-test('tests that some data is returned', () => {
-  expect(dp.loadUnemploymentTrend()).toBeDefined();
-});
-
-test('tests getLasDayColumn on the correct data', () => {
-    expect(dp.getLasDayColumn([{"date":'4/1/65'}])).toBe('4/1/20');
-});
